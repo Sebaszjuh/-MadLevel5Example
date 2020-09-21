aa5627e14af4b681ba173bf05825232bf84e3b1c
griends1 <Steelpan1>
2020-09-21 21:12:25 +0200

    set-up, copy of level4example

---
+++ app/src/main/java/com/example/madlevel5example/DAO/ReminderDAO.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel5example.DAO
+
+import androidx.room.*
+import com.example.madlevel5example.Model.Reminder
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    suspend fun getAllReminders(): List<Reminder>
+
+    @Insert
+    suspend fun insertReminder(reminder: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminder: Reminder)
+
+    @Update
+    suspend fun updateReminder(reminder: Reminder)
+
+}
---
+++ app/src/main/java/com/example/madlevel5example/Repository/ReminderRepository.kt
@@ -0,0 +1,31 @@
+package com.example.madlevel5example.Repository
+
+import android.content.Context
+import com.example.madlevel5example.Model.Reminder
+import com.example.madlevel5example.DAO.ReminderDao
+
+class ReminderRepository(context: Context) {
+
+    private var reminderDAO: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDAO = reminderRoomDatabase!!.reminderDao()
+    }
+
+    suspend fun getAllReminders(): List<Reminder> {
+        return reminderDAO.getAllReminders()
+    }
+
+    suspend fun insertReminder(reminder: Reminder) {
+        reminderDAO.insertReminder(reminder)
+    }
+
+    suspend fun deleteReminder(reminder: Reminder) {
+        reminderDAO.deleteReminder(reminder)
+    }
+
+    suspend fun updateReminder(reminder: Reminder) {
+        reminderDAO.updateReminder(reminder)
+    }
+}
